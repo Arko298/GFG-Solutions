156259e71d2f4048703b03956a14b6bfa34e01a3
Sayantan Choudhury <123821730+Arko298@users.noreply.github.com>
2024-10-08 11:55:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Largest Pair Sum/largest-pair-sum.py
@@ -0,0 +1,48 @@
+
+from typing import List
+
+
+class Solution:
+    def pairsum(self, arr : List[int]) -> int:
+        # code here
+        arr.sort()
+        summ=0
+        for i in arr:
+            if i != i+1:
+                summ=arr[-1]+arr[-2]
+            return summ
+        return -1 #if all are same
+                
+        
+
+
+
+#{ 
+ # Driver Code Starts
+class IntArray:
+
+    def __init__(self) -> None:
+        pass
+
+    def Input(self, n):
+        arr = [int(i) for i in input().strip().split()]  #array input
+        return arr
+
+    def Print(self, arr):
+        for i in arr:
+            print(i, end=" ")
+        print()
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+
+        arr = list(map(int, input().strip().split()))
+
+        obj = Solution()
+        res = obj.pairsum(arr)
+
+        print(res)
+
+# } Driver Code Ends
